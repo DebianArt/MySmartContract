7ec5b9b14316c573321128c9388801f92f8948bd
DebianArt <91970458+DebianArt@users.noreply.github.com>
2022-02-03 17:05:23 +0100

    Update pruebaImport.sol

--- pruebaImport.sol
+++ pruebaImport.sol
@@ -1,1 +1,13 @@
+pragma solidity ^0.4.0;
 
+contract SimpleStorage{
+  uint storeData;
+  
+    function set(uint x){
+        storeData = x;
+    }
+    
+    function get() constante returns(uint){
+      return storeData
+    }
+}
